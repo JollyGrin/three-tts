f4bc160dfd41d612ff2d7fceb726f35c6f94ab38
JollyGrin <lol@dean.lol>
2025-03-10 15:39:48 +0100

    add drag store

--- src/lib/store/dragStore.svelte.ts
+++ src/lib/store/dragStore.svelte.ts
@@ -7,3 +7,17 @@ export const dragStore = writable({
 	isDragging: string | null;
 	isHovered: string | null;
 });
+
+export function dragStart(id: string) {
+	dragStore.set({
+		isDragging: id,
+		isHovered: id
+	});
+}
+
+export function dragEnd() {
+	dragStore.set({
+		isDragging: null,
+		isHovered: null
+	});
+}
