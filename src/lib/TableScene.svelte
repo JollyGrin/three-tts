5b930b9a455892d2c8a4f1ef4b5f722b37a33e9b
JollyGrin <lol@dean.lol>
2025-03-10 15:39:55 +0100

    add id

--- src/lib/TableScene.svelte
+++ src/lib/TableScene.svelte
@@ -4,10 +4,14 @@
 	import { OrbitControls } from '@threlte/extras';
 	import Table from './Table.svelte';
 	import Card from './Card.svelte';
+	import { dragStore } from '$lib/store/dragStore.svelte';
+
+	const isDragging = $derived(!!$dragStore.isDragging);
+	$inspect(isDragging);
 </script>
 
 <T.PerspectiveCamera makeDefault position={[0, 10, 10]} fov={50}>
-	<OrbitControls enableDamping maxPolarAngle={Math.PI / 2 - 0.1} />
+	<OrbitControls enableRotate={!isDragging} enableDamping maxPolarAngle={Math.PI / 2 - 0.1} />
 </T.PerspectiveCamera>
 
 <T.DirectionalLight position={[3, 10, 10]} intensity={1.5} />
@@ -16,7 +20,7 @@
 <World>
 	<Table />
 	<!-- Add some test cards -->
-	<Card position={[0, 2, 0]} />
-	<Card position={[1, 3, 1]} />
-	<Card position={[-1, 4, -1]} />
+	<Card position={[0, 2, 0]} id="a" />
+	<Card position={[1, 3, 1]} id="b" />
+	<Card position={[-1, 4, -1]} id="c" />
 </World>
