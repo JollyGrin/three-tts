633797a91e44f612c48b3b610152186df2c2db7a
JollyGrin <lol@dean.lol>
2025-03-10 15:07:45 +0100

    working glow

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,32 +1,37 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
 	import { Collider, RigidBody } from '@threlte/rapier';
-	import { Spring } from 'svelte/motion';
-	import { Vector3 } from 'three';
 
 	let { position = [0, 0, 0] as [number, number, number] } = $props();
 
-	let rigidBody = $state();
 	let isDragging = $state(false);
+	let isHovered = $state(false);
+	let emissiveIntensity = $state(0);
 
-	const targetPosition = new Spring(position, {
-		stiffness: 0.1,
-		damping: 0.8
+	$effect(() => {
+		emissiveIntensity = isHovered ? 0.5 : 0;
 	});
 
 	function handleDragStart() {
 		isDragging = true;
-		// rigidBody.setTranslation(new Vector3(...$targetPosition), true);
 	}
 
 	function handleDrag(event: { delta: [number, number] }) {
 		const [x, z] = event.delta;
-		targetPosition.update((pos) => [pos[0] + x * 0.01, pos[1], pos[2] + z * 0.01]);
-		// rigidBody.setTranslation(new Vector3(...$targetPosition), true);
+		position = [position[0] + x * 0.01, position[1], position[2] + z * 0.01];
 	}
 
 	function handleDragEnd() {
 		isDragging = false;
+	}
+
+	function handlePointerEnter() {
+		console.log('hi');
+		isHovered = true;
+	}
+
+	function handlePointerLeave() {
+		isHovered = false;
 	}
 </script>
 
@@ -41,13 +46,14 @@
 		<T.Mesh
 			rotation.x={-Math.PI / 2}
 			position.y={0.01}
-			on:pointerdown={handleDragStart}
-			on:pointermove={handleDrag}
-			on:pointerup={handleDragEnd}
-			on:pointerleave={handleDragEnd}
+			onpointerdown={handleDragStart}
+			onpointermove={handleDrag}
+			onpointerup={handleDragEnd}
+			onpointerleave={handlePointerLeave}
+			onpointerenter={handlePointerEnter}
 		>
 			<T.PlaneGeometry args={[1.4, 2]} />
-			<T.MeshStandardMaterial color="white" />
+			<T.MeshStandardMaterial color="white" emissive="#4444ff" {emissiveIntensity} />
 		</T.Mesh>
 	</RigidBody>
 </T.Group>
