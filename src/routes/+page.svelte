2e0e3b166d701fcec6d558b6ab706b83db1a7e00
JollyGrin <lol@dean.lol>
2025-03-12 13:41:17 +0100

    add helper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,19 @@
 	import { objectStore, updateCardState } from '$lib/store/objectStore.svelte';
 	import { dragStore } from '$lib/store/dragStore.svelte';
 
+	function flipCard() {
+		const id = $dragStore.isHovered || $dragStore.isDragging;
+		if (!id) return;
+
+		const card = $objectStore[id as string];
+		const isFlipped = card.rotation[0] === 180;
+		updateCardState(id as string, card.position, card.faceImageUrl, [isFlipped ? 0 : 180, 0, 0]);
+	}
 	function handleKeyDown(event: KeyboardEvent) {
 		const id = $dragStore.isHovered || $dragStore.isDragging;
 		if (!id) return event.preventDefault();
 
-		const card = $objectStore[id as string];
-		if (event.code === 'KeyF') {
-			const isFlipped = card.rotation[0] === 180;
-			updateCardState(id as string, card.position, card.faceImageUrl, [isFlipped ? 0 : 180, 0, 0]);
-		}
+		if (event.code === 'KeyF') flipCard();
 	}
 
 	function handleKeyUp(event: KeyboardEvent) {
@@ -24,7 +28,16 @@
 
 <svelte:window on:keydown|preventDefault={handleKeyDown} on:keyup|preventDefault={handleKeyUp} />
 
-<div class="h-screen w-screen">
+<div
+	class="fixed top-1 right-1 z-50 flex w-fit scale-80 gap-1 rounded bg-gray-50/10 px-2 py-[2px] opacity-30 md:top-4 md:right-4 md:scale-100"
+>
+	<button onclick={flipCard}>
+		<span class="rounded border-b-[2px] border-b-gray-700 bg-gray-200 px-2"> f </span>
+		<span class="text-white"> Flip card </span>
+	</button>
+</div>
+
+<div class="h-screen w-screen overflow-clip">
 	<Canvas>
 		<TableScene />
 	</Canvas>
