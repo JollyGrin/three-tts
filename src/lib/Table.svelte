de08b6ca8267bf2d5eb58fd8986b170189d34869
JollyGrin <lol@dean.lol>
2025-03-27 16:12:44 +0100

    fix position of grid to center it

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -76,21 +76,24 @@
 	});
 </script>
 
-<T.Mesh position.y={0.259} position.x={9.5} rotation.x={-DEG2RAD * 90} side={0}>
-	<T.PlaneGeometry args={[3, 12]} />
-</T.Mesh>
+<T.Group position.x={-0.5}>
+	<T.Mesh position.y={0.259} position.x={9.5} rotation.x={-DEG2RAD * 90} side={0}>
+		<T.PlaneGeometry args={[3, 12]} />
+		<T.MeshBasicMaterial color="#35654d" />
+	</T.Mesh>
 
-<Grid
-	scale={3}
-	fadeDistance={1000}
-	fadeStrength={0}
-	position.y={0.2569}
-	position.x={2}
-	cellSize={1}
-	cellThickness={3}
-	gridSize={[6, 4]}
-	sectionSize={0}
-/>
+	<Grid
+		scale={3}
+		fadeDistance={1000}
+		fadeStrength={0}
+		position.y={0.2569}
+		position.x={2}
+		cellSize={1}
+		cellThickness={3}
+		gridSize={[6, 4]}
+		sectionSize={0}
+	/>
+</T.Group>
 
 <Grid
 	position.y={0.255}
