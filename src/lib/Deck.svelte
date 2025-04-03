976edb6735cfa1456f266791cb1b2ec871b88606
JollyGrin <lol@dean.lol>
2025-04-03 03:26:34 +0200

    use store to update values in deck

--- src/lib/Deck.svelte
+++ src/lib/Deck.svelte
@@ -4,18 +4,17 @@
 	import { deckStore } from '$lib/store/deckStore.svelte';
 
 	let {
-		id = '',
-		position = [0, 0, 0],
-		rotation = [0, 0, 0],
-		isFaceUp = false
+		id = ''
 	}: {
 		id: string;
-		position?: [number, number, number];
-		rotation?: [number, number, number];
-		isFaceUp?: boolean;
 	} = $props();
 
 	const deck = $derived($deckStore[id] ?? {});
+	const position = $derived(deck.position ?? [0, 0, 0]);
+	const rotation = $derived(deck.rotation ?? [0, 0, 0]);
+	const isFaceUp = $derived(deck.isFaceUp ?? false); // true = cards[0] is top, false = cards[cards.length - 1] is top
+	const lastCardImage = $derived(deck?.cards?.[0].faceImageUrl ?? '');
+	const displayedImage = $derived(isFaceUp ? lastCardImage : '/s-back.jpg');
 </script>
 
 <T.Group {position} {rotation}>
@@ -29,7 +28,7 @@
 	</Billboard>
 	<T.Mesh castShadow receiveShadow rotation.x={-Math.PI / 2} position.y={0.205}>
 		<T.PlaneGeometry args={[1.4, 2]} />
-		<ImageMaterial url={'/s-back.jpg'} side={2} radius={0.1} />
+		<ImageMaterial url={displayedImage} side={2} radius={0.1} />
 	</T.Mesh>
 	<T.Mesh>
 		<T.BoxGeometry args={[1.4, 0.4, 2]} />
