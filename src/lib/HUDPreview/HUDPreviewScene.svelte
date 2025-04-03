e49aeb0d642e385eed1ce41800fd36e1201574b9
JollyGrin <lol@dean.lol>
2025-04-03 14:07:12 +0200

    fix up the preview glitch

--- src/lib/HUDPreview/HUDPreviewScene.svelte
+++ src/lib/HUDPreview/HUDPreviewScene.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
 	import { dragStore } from '$lib/store/dragStore.svelte';
-	import { objectStore } from '$lib/store/objectStore.svelte';
 	import { useViewport, interactivity } from '@threlte/extras';
 	import PreviewCard from './PreviewCard.svelte';
 
@@ -14,16 +13,14 @@
 	const trayHeight = $derived($viewport.height / 2); // Adjust height as needed
 	const trayX = $derived(-$viewport.width / 2 + trayWidth / 2);
 	const trayY = $derived(-$viewport.height / 2 + trayHeight / 2);
-
-	let hoveredCard = $objectStore[$dragStore.isHovered ?? ''];
 </script>
 
 <T.OrthographicCamera makeDefault zoom={80} position={[0, 0, 10]} />
 <T.AmbientLight intensity={Math.PI / 2} />
 <T.PointLight position={[10, 10, 10]} decay={0} intensity={Math.PI * 2} />
 
-{#if $dragStore.isHovered && $dragStore.isPreview}
+{#if !!$dragStore.isHovered && $dragStore.isPreview}
 	<T.Group position={[-trayX, trayY, 0] as [number, number, number]}>
-		<PreviewCard id={$dragStore.isHovered} />
+		<PreviewCard id={$dragStore.isHovered ?? ''} />
 	</T.Group>
 {/if}
