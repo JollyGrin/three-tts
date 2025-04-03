e0b0c964308fe394d282e4c065762c12cae90a76
JollyGrin <lol@dean.lol>
2025-04-03 03:49:13 +0200

    draw mechanic

--- src/lib/Deck.svelte
+++ src/lib/Deck.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
-	import { Text, Billboard, ImageMaterial } from '@threlte/extras';
+	import { Text, Billboard, ImageMaterial, interactivity } from '@threlte/extras';
 	import { deckStore } from '$lib/store/deckStore.svelte';
+
+	interactivity();
 
 	let {
 		id = ''
@@ -15,9 +17,14 @@
 	const isFaceUp = $derived(deck.isFaceUp ?? false); // true = cards[0] is top, false = cards[cards.length - 1] is top
 	const lastCardImage = $derived(deck?.cards?.[0].faceImageUrl ?? '');
 	const displayedImage = $derived(isFaceUp ? lastCardImage : '/s-back.jpg');
+
+	const draw = (e: PointerEvent) => {
+		e.stopPropagation();
+		deckStore.drawFromTop(id);
+	};
 </script>
 
-<T.Group {position} {rotation}>
+<T.Group {position} {rotation} onpointerup={draw}>
 	<Billboard>
 		<Text
 			fontSize={0.5}
@@ -26,10 +33,25 @@
 			anchorX="center"
 		/>
 	</Billboard>
-	<T.Mesh castShadow receiveShadow rotation.x={-Math.PI / 2} position.y={0.205}>
-		<T.PlaneGeometry args={[1.4, 2]} />
-		<ImageMaterial url={displayedImage} side={2} radius={0.1} />
-	</T.Mesh>
+	{#if deck.cards.length > 0}
+		<T.Mesh castShadow receiveShadow rotation.x={-Math.PI / 2} position.y={0.205}>
+			<T.PlaneGeometry args={[1.4, 2]} />
+			{#key displayedImage}
+				<ImageMaterial url={displayedImage} side={2} radius={0.1} />
+			{/key}
+		</T.Mesh>
+	{/if}
+
+	<!-- PRELOAD THE NEXT DISCARD IMAGE -->
+	{#if isFaceUp && deck.cards.length > 1}
+		<T.Mesh>
+			<T.PlaneGeometry args={[0, 0]} />
+			{#key deck.cards[1]?.faceImageUrl}
+				<ImageMaterial url={deck.cards[1]?.faceImageUrl} side={2} radius={0.1} opacity={0} />
+			{/key}
+		</T.Mesh>
+	{/if}
+
 	<T.Mesh>
 		<T.BoxGeometry args={[1.4, 0.4, 2]} />
 		<T.MeshBasicMaterial color="black" />
--- src/lib/store/deckStore.svelte.ts
+++ src/lib/store/deckStore.svelte.ts
@@ -31,6 +31,19 @@ function updateDeck(id: string, updatedState: Partial<DeckDTO>) {
 }
 
 /**
+ * Draws from the top of the deck
+ * Follows LIFO (Last In First Out)
+ * When facedown (like a deck of cards), the top card = cards.length - 1
+ * When faceup (like a visible discard pile), the top card = cards[0]
+ * */
+function drawFromTop(id: string) {
+	const { cards, isFaceUp, ...deck } = get(decks)[id];
+
+	const card = isFaceUp ? cards.shift() : cards.pop();
+	updateDeck(id, { cards, isFaceUp, ...deck });
+}
+
+/**
  * How many cards are in the deck
  * */
 function getDeckLength(id: string) {
@@ -40,5 +53,6 @@ function getDeckLength(id: string) {
 export const deckStore = {
 	...decks,
 	updateDeck,
-	getDeckLength
+	getDeckLength,
+	drawFromTop
 };
