eb81371f941dd3cd58f69fcaef408cb865a275b9
JollyGrin <lol@dean.lol>
2025-04-06 16:19:33 +0200

    remove unused

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,8 +33,7 @@
 	});
 	const showInitDeck = $derived($playerStore[playerStore.getMe()?.id]?.deckIds.length === 0);
 
-	$inspect('xxxxx - object store', $objectStore);
-	$inspect('xxxxx - player store', $playerStore);
+	$inspect('xxxxx - local player store', $playerStore);
 </script>
 
 <svelte:head>
