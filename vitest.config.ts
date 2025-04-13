c04cc36b8813537c29085ef85ca27d6b65ad9497
JollyGrin <lol@dean.lol>
2025-04-13 17:27:47 +0200

    add tests

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: ['**/*.d.ts', '**/*.test.ts', '**/*.spec.ts']
+    }
+  }
+});
