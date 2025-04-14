8b5bfaeb10c58a6c8addcc52acef8bc2d43d8115
JollyGrin <lol@dean.lol>
2025-04-14 03:35:36 +0200

    replace stores

--- src/lib/Deck.svelte
+++ src/lib/Deck.svelte
@@ -2,27 +2,25 @@
 	import * as THREE from 'three';
 	import { T } from '@threlte/core';
 	import { Text, Billboard, ImageMaterial, interactivity } from '@threlte/extras';
-	import { degrees, seatStore } from '$lib/store/seatStore.svelte';
-	import { objectStore } from '$lib/store/objectStore.svelte';
 	import { dragStart, dragStore, setDeckHover } from '$lib/store/dragStore.svelte';
-	import { deckStore } from '$lib/store/deckStore.svelte';
+	import { degrees } from '$lib/utils/constants-rotation';
 	import { DEG2RAD } from 'three/src/math/MathUtils.js';
 	import { getStaticResourceUrl } from './utils/image';
+	import { gameStore } from './store/game/gameStore.svelte';
+	import { gameActions } from './store/game/actions';
 
 	interactivity();
 
-	let {
-		id = ''
-	}: {
-		id: string;
-	} = $props();
+	let { id = '' }: { id: string } = $props();
 
 	const deckBackImage = getStaticResourceUrl('/s-back.jpg');
-	const deck = $derived($deckStore[id] ?? {});
-	const position = $derived(deck.position ?? [0, 0, 0]);
-	const rotation = $derived(deck.rotation ?? [0, 0, 0]);
+	const deck = $derived($gameStore?.decks?.[id] ?? {});
+
+	const cards = $derived($gameStore?.decks?.[id]?.cards ?? []);
+	const position: [number, number, number] = $derived(deck.position ?? [0, 0, 0]);
+	const rotation: [number, number, number] = $derived(deck.rotation ?? [0, 0, 0]);
 	const isFaceUp = $derived(deck.isFaceUp ?? false); // true = cards[0] is top, false = cards[cards.length - 1] is top
-	const lastCardImage = $derived(deck?.cards?.[0].faceImageUrl ?? '');
+	const lastCardImage = $derived(cards?.[0].faceImageUrl ?? '');
 	const displayedImage = $derived(isFaceUp ? lastCardImage : deckBackImage);
 
 	const isHovered = $derived(id === $dragStore.isDeckHovered);
@@ -30,18 +28,28 @@
 	function handleDragStart(e: PointerEvent) {
 		e.stopPropagation();
 		const { x = 0, z = 0 } = $dragStore.intersectionPoint as THREE.Vector3;
-		const card = deckStore.drawFromTop(id);
+		const card = gameActions.drawFromTop(id);
 		if (!card) return console.warn('No card drawn');
 
 		const rotX = isFaceUp ? 0 : 180;
 		const rotY = 0;
-		const rotZ = -degrees[$seatStore.seat] / DEG2RAD;
-		objectStore.updateCard(card.id, {
-			position: [x, 2.5, z],
-			rotation: [rotX, rotY, rotZ],
-			faceImageUrl: card.faceImageUrl,
-			backImageUrl: card.backImageUrl ?? deckBackImage
+		const rotZ = -degrees[gameActions?.getMySeat()] / DEG2RAD;
+		gameStore.updateState({
+			cards: {
+				[card.id]: {
+					...card,
+					position: [x, 2.5, z],
+					rotation: [rotX, rotY, rotZ],
+					backImageUrl: card.backImageUrl ?? deckBackImage
+				}
+			}
 		});
+		// objectStore.updateCard(card.id, {
+		// 	position: [x, 2.5, z],
+		// 	rotation: [rotX, rotY, rotZ],
+		// 	faceImageUrl: card.faceImageUrl,
+		// 	backImageUrl: card.backImageUrl ?? deckBackImage
+		// });
 
 		dragStart(card.id, 2.5);
 	}
@@ -57,12 +65,12 @@
 	<Billboard>
 		<Text
 			fontSize={0.5}
-			text={(deck.cards ?? [])?.length?.toString() ?? '0'}
+			text={(cards ?? [])?.length?.toString() ?? '0'}
 			position={[0, 1.75, 0]}
 			anchorX="center"
 		/>
 	</Billboard>
-	{#if deck.cards.length > 0}
+	{#if cards?.length > 0}
 		<T.Mesh castShadow receiveShadow rotation.x={-Math.PI / 2} position.y={0.205}>
 			<T.PlaneGeometry args={[1.4, 2]} />
 			{#key displayedImage}
@@ -72,11 +80,11 @@
 	{/if}
 
 	<!-- PRELOAD THE NEXT DISCARD IMAGE -->
-	{#if isFaceUp && deck.cards.length > 1}
+	{#if isFaceUp && cards.length > 1}
 		<T.Mesh>
 			<T.PlaneGeometry args={[0, 0]} />
-			{#key deck?.cards[1]?.faceImageUrl}
-				<ImageMaterial url={deck?.cards[1]?.faceImageUrl} side={2} radius={0.1} opacity={0} />
+			{#key cards[1]?.faceImageUrl}
+				<ImageMaterial url={cards[1]?.faceImageUrl} side={2} radius={0.1} opacity={0} />
 			{/key}
 		</T.Mesh>
 	{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import { Canvas } from '@threlte/core';
 	import TableScene from '$lib/TableScene.svelte';
-	import { cardTransforms } from '$lib/utils/transforms/card';
-	import { seatStore, setSeat } from '$lib/store/seatStore.svelte';
-	import { playerStore } from '$lib/store/playerStore.svelte';
 	import { cameraTransforms } from '$lib/utils/transforms/camera';
 	import { onMount } from 'svelte';
-	import { deckStore } from '$lib/store/deckStore.svelte';
 	import { initWrappers } from '$lib/websocket/storeIntegration';
 	import { initWebsocket } from '$lib/websocket';
 	import { gameStore } from '$lib/store/game/gameStore.svelte';
+	import { gameActions } from '$lib/store/game/actions';
 
 	$inspect('cards:', $gameStore?.cards);
 	$inspect('players:', $gameStore?.players);
@@ -17,9 +14,9 @@
 
 	function handleKeyDown(event: KeyboardEvent) {
 		if (event.code === 'Space') cameraTransforms.togglePreviewHud(true);
-		if (event.code === 'KeyF') cardTransforms.flip();
-		if (event.code === 'KeyT') cardTransforms.tap();
-		if (event.code === 'KeyR') cardTransforms.tap(true);
+		if (event.code === 'KeyF') gameActions.flipCard();
+		if (event.code === 'KeyT') gameActions.tapCard();
+		if (event.code === 'KeyR') gameActions.tapCard(true);
 	}
 
 	function handleKeyUp(event: KeyboardEvent) {
@@ -29,15 +26,12 @@
 	initWrappers();
 
 	onMount(() => {
-		if (playerStore.getMe() !== undefined) return;
-		playerStore.addPlayer(undefined, true); // generate new player with random id and assign as me
-
+		if (gameActions.getMe() === undefined) return gameActions.addPlayer(); // generate new player with random id and assign as me
 		initWebsocket();
 	});
 
-	const showInitDeck = $derived($playerStore[playerStore.getMe()?.id]?.deckIds.length === 0);
+	const showInitDeck = $derived(gameActions.getMyDecks().length === 0);
 
-	let deckInput = $state('');
 	async function fetchDeck() {
 		const response = await fetch(
 			'https://corsproxy.innkeeper1.workers.dev/?url=https://curiosa.io/api/decks/clso3lngx007lhb600v843gd7'
@@ -57,8 +51,10 @@
 <div
 	class="fixed top-1 right-1 z-50 flex w-fit scale-80 gap-4 rounded bg-gray-50/10 px-2 py-[2px] text-xs opacity-30 md:top-4 md:right-4 md:scale-100"
 >
-	<button onclick={() => setSeat()}>
-		<span class="bg-gray-400 px-2">{$seatStore.seat}</span>
+	<button onclick={() => gameActions.setSeat()}>
+		<span class="bg-gray-400 px-2"
+			>{$gameStore?.players?.[gameActions?.getMe()?.id ?? 0]?.seat}</span
+		>
 		<span class="text-white"> Next Seat </span>
 	</button>
 
@@ -69,36 +65,36 @@
 		<span class="text-white"> Preview hovered card </span>
 	</button>
 
-	<button onclick={cardTransforms.flip}>
+	<button onclick={() => gameActions.flipCard()}>
 		<span class="rounded border-b-[2px] border-b-gray-700 bg-gray-200 px-2"> f </span>
 		<span class="text-white"> Flip card </span>
 	</button>
 
-	<button onclick={() => cardTransforms.tap()}>
+	<button onclick={() => gameActions.tapCard()}>
 		<span class="rounded border-b-[2px] border-b-gray-700 bg-gray-200 px-2"> t </span>
 		<span class="text-white"> Tap card </span>
 	</button>
 
-	<button onclick={() => cardTransforms.tap(true)}>
+	<button onclick={() => gameActions?.tapCard(true)}>
 		<span class="rounded border-b-[2px] border-b-gray-700 bg-gray-200 px-2"> r </span>
 		<span class="text-white"> Reverse Tap card </span>
 	</button>
 </div>
 
-<div class="fixed right-1 bottom-1 z-50 flex w-fit rounded bg-white p-2">
-	<input />
-	<button onclick={fetchDeck}>init deck</button>
-</div>
+<!-- <div class="fixed right-1 bottom-1 z-50 flex w-fit rounded bg-white p-2"> -->
+<!-- 	<input /> -->
+<!-- 	<button onclick={fetchDeck}>init deck</button> -->
+<!-- </div> -->
 
 {#if true && showInitDeck}
 	<button
-		class="fixed right-1 bottom-16 z-50 flex w-fit rounded bg-white p-2"
+		class="fixed right-1 bottom-6 z-50 flex w-fit rounded bg-white p-2"
 		onclick={() => {
-			deckStore.initDeck({
+			gameActions.initDeck({
 				isFaceUp: false
 			});
 
-			deckStore.initDeck({
+			gameActions.initDeck({
 				isFaceUp: true
 			});
 		}}>init deck</button
